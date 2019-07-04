e784415287f7d3bec8253600f4f58987c818172c
景阳 <2427065324@qq.com>
2019-07-04 17:59:57 +0800

    123

--- js.js
+++ js.js
@@ -1,15 +1,23 @@
-var a=10;
-var b=1;
-function fn(){
-    var a=12;
+var a = 10;
+var b = 1;
+function fn() {
+    var a = 12;
     console.log(a);
 }
 fn()
 console.log(a)
 var c;
 console.log(c);
-c=a;
-function fn1(){
-  console.log(c);  
+c = a;
+function fn1() {
+    console.log(c);
 }
 fn1()
+var d = {}
+function fn2() {
+    var e = c;
+    console.log(e)
+}
+fn2()
+e = 12;
+console.log(e)
